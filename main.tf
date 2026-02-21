dbca014c4de6ddb198ebd8d0289ca64a54d5cd4f
poddars13 <sp.souravpoddar@gmail.com>
2026-02-21 21:28:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,33 +1,33 @@
-resource "azurerm_resource_group" "terraformgroup" {
-  name     = "TEST-RG"
+resource "azurerm_resource_group" "myterraformgroup" {
+  name     = "myResourceGroup"
   location = "CentralIndia"
 }
 # Create virtual network
-resource "azurerm_virtual_network" "terraformnetwork" {
-  name                = "Test-Vnet"
+resource "azurerm_virtual_network" "myterraformnetwork" {
+  name                = "myVnet"
   address_space       = ["10.0.0.0/16"]
-  location            = "SouthIndia"
-  resource_group_name = azurerm_resource_group.terraformgroup.name
+  location            = "CentralIndia"
+  resource_group_name = azurerm_resource_group.myterraformgroup.name
 }
 # Create subnet
-resource "azurerm_subnet" "terraformsubnet" {
-  name                 = "TestSubnet"
-  resource_group_name  = azurerm_resource_group.terraformgroup.name
-  virtual_network_name = azurerm_virtual_network.terraformnetwork.name
+resource "azurerm_subnet" "myterraformsubnet" {
+  name                 = "mySubnet"
+  resource_group_name  = azurerm_resource_group.myterraformgroup.name
+  virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 # Create public IPs
-resource "azurerm_public_ip" "terraformpublicip" {
-  name                = "TestPublicIP"
+resource "azurerm_public_ip" "myterraformpublicip" {
+  name                = "myPublicIP"
   location            = "CentralIndia"
-  resource_group_name = azurerm_resource_group.terraformgroup.name
+  resource_group_name = azurerm_resource_group.myterraformgroup.name
   allocation_method   = "Static"
 }
 # Create Network Security Group and rule
-resource "azurerm_network_security_group" "terraformnsg" {
-  name                = "TestNetworkSecurityGroup"
+resource "azurerm_network_security_group" "myterraformnsg" {
+  name                = "myNetworkSecurityGroup"
   location            = "CentralIndia"
-  resource_group_name = azurerm_resource_group.terraformgroup.name
+  resource_group_name = azurerm_resource_group.myterraformgroup.name
 
   security_rule {
     name                       = "SSH"
@@ -42,30 +42,30 @@ resource "azurerm_network_security_group" "terraformnsg" {
   }
 }
 # Create network interface
-resource "azurerm_network_interface" "terraformnic" {
-  name                = "TestNIC"
+resource "azurerm_network_interface" "myterraformnic" {
+  name                = "myNIC"
   location            = "CentralIndia"
-  resource_group_name = azurerm_resource_group.terraformgroup.name
+  resource_group_name = azurerm_resource_group.myterraformgroup.name
 
   ip_configuration {
     name                          = "myNicConfiguration"
-    subnet_id                     = azurerm_subnet.terraformsubnet.id
+    subnet_id                     = azurerm_subnet.myterraformsubnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.terraformpublicip.id
+    public_ip_address_id          = azurerm_public_ip.myterraformpublicip.id
   }
 }
 # Connect the security group to the network interface
 resource "azurerm_network_interface_security_group_association" "example" {
-  network_interface_id      = azurerm_network_interface.terraformnic.id
-  network_security_group_id = azurerm_network_security_group.terraformnsg.id
+  network_interface_id      = azurerm_network_interface.myterraformnic.id
+  network_security_group_id = azurerm_network_security_group.myterraformnsg.id
 }
 # Create virtual machine
-resource "azurerm_linux_virtual_machine" "terraformvm" {
-  name                  = "TestVM"
+resource "azurerm_linux_virtual_machine" "myterraformvm" {
+  name                  = "myVM"
   location              = "CentralIndia"
-  resource_group_name   = azurerm_resource_group.terraformgroup.name
-  network_interface_ids = [azurerm_network_interface.terraformnic.id]
-  size                  = "Standard_A2_v2"
+  resource_group_name   = azurerm_resource_group.myterraformgroup.name
+  network_interface_ids = [azurerm_network_interface.myterraformnic.id]
+  size                  = "Standard_B1ls"
 
   os_disk {
     name                 = "myOsDisk"
